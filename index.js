3b49af2dd8e5d96c0b4849d320834ccaa1b2d080
Jean-Christophe Hoelt <hoelt@fovea.cc>
2017-08-02 09:15:39 +0300

    Fix eslint

--- index.js
+++ index.js
@@ -1,71 +1,73 @@
+'use strict';
+
 require('coffee-script/register');
 
 // Use New Relic if LICENSE_KEY has been specified.
 if (process.env.NEW_RELIC_LICENSE_KEY) {
-    if (!process.env.NEW_RELIC_APP_NAME) {
-        var pk = require('./package.json');
-        process.env.NEW_RELIC_APP_NAME = pk.api;
-    }
-    require('newrelic');
+  if (!process.env.NEW_RELIC_APP_NAME) {
+    const pk = require('./package.json');
+    process.env.NEW_RELIC_APP_NAME = pk.api;
+  }
+  require('newrelic');
 }
 
-var cluster = require("cluster");
-var log = require("./src/log");
-var config = require('./config');
+const cluster = require('cluster');
+const log = require('./src/log');
+const config = require('./config');
 
 if (cluster.isMaster) {
 
-    // master
-    log.info("starting up...", {env: process.env, config: config});
+  // master
+  log.info('starting up...', {env: process.env, config: config});
+  cluster.fork();
+  cluster.fork();
+  cluster.on('disconnect', function (/*worker*/) {
+    log.error('disconnect!');
     cluster.fork();
-    cluster.fork();
-    cluster.on("disconnect", function(/*worker*/) {
-        log.error("disconnect!");
-        cluster.fork();
-    });
+  });
 }
 else {
-    var main = require("./src/main");
-    var server = require('./src/server')();
+  const main = require('./src/main');
+  const server = require('./src/server')();
 
-    // Intitialize backend, add routes
-    main(config.routePrefix, server);
+  // Intitialize backend, add routes
+  main(config.routePrefix, server);
 
-    // Handle uncaughtException, kill the worker
-    server.on('uncaughtException', function (req, res, route, err) {
+  // Handle uncaughtException, kill the worker
+  server.on('uncaughtException', function (req, res, route, err) {
 
-        // Log the error
-        log.error(err);
+    // Log the error
+    log.error(err);
 
-        // Note: we're in dangerous territory!
-        // By definition, something unexpected occurred,
-        // which we probably didn't want.
-        // Anything can happen now!  Be very careful!
-        try {
-            // make sure we close down within 30 seconds
-            setTimeout(function() {
-                process.exit(1);
-            }, 30000);
+    // Note: we're in dangerous territory!
+    // By definition, something unexpected occurred,
+    // which we probably didn't want.
+    // Anything can happen now!  Be very careful!
+    try {
+      // make sure we close down within 30 seconds
+      setTimeout(function () {
+        process.exit(1);
+      }, 30000);
 
-            // stop taking new requests
-            server.close();
+      // stop taking new requests
+      server.close();
 
-            // Let the master know we're dead.  This will trigger a
-            // 'disconnect' in the cluster master, and then it will fork
-            // a new worker.
-            cluster.worker.disconnect();
+      // Let the master know we're dead.  This will trigger a
+      // 'disconnect' in the cluster master, and then it will fork
+      // a new worker.
+      cluster.worker.disconnect();
 
-            var InternalError = require('restify').InternalError;
-            res.send(new InternalError(err, err.message || 'unexpected error'));
-        }
-        catch (err2) {
-            log.error("Error sending 500!");
-            log.error(err2);
-        }
-    });
+      const InternalError = require('restify').InternalError;
+      res.send(new InternalError(err, err.message || 'unexpected error'));
+    }
+    catch (err2) {
+      log.error('Error sending 500!');
+      log.error(err2);
+    }
+  });
 
-    // Start the server
-    server.listen(config.port, function() {
-        log.info(server.name + " listening at " + server.url);
-    });
+  // Start the server
+  server.listen(config.port, function () {
+    log.info(server.name + ' listening at ' + server.url);
+  });
 }
